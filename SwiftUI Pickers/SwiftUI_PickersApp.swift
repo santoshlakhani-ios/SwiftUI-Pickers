6997291edbb8d73f9e9ada61a7e72e57104b63f6
Santosh Lakhani <lakhanisantosh@gmail.com>
2025-08-06 19:58:51 +0530

    Initial Commit

---
+++ SwiftUI Pickers/SwiftUI_PickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PickersApp.swift
+//  SwiftUI Pickers
+//
+//  Created by Deepak Kapdi on 06/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI PickersTests/SwiftUI_PickersTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUI_PickersTests.swift
+//  SwiftUI PickersTests
+//
+//  Created by Deepak Kapdi on 06/08/25.
+//
+
+import Testing
+
+struct SwiftUI_PickersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
